a3f2ac55be931d91d7f11e4aa13c2c24f6a0fa9c
Pavani1711 <133211000+Pavani1711@users.noreply.github.com>
2023-06-11 18:52:31 +0530

    Add files via upload

---
+++ e learning website/scriptss.js
@@ -0,0 +1,18 @@
+let menu = document.querySelector('#menu');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+}
